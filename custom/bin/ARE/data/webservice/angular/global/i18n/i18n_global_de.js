9740f7f465170510179b2b59049b28e0cabd55ce
Benjamin Klaus <klues@live.de>
2017-03-22 17:05:48 +0100

    added disclaimer

--- custom/bin/ARE/data/webservice/angular/global/i18n/i18n_global_de.js
+++ custom/bin/ARE/data/webservice/angular/global/i18n/i18n_global_de.js
@@ -4,5 +4,7 @@ asterics.i18n_global_de = {
     i18n_lang_en: 'Englisch',
     i18n_available_apps: 'Verfügbare Apps',
     i18n_you_are_here: 'Sie befinden sich hier:',
-    i18n_back: 'Zurück'
+    i18n_back: 'Zurück',
+    i18n_disclaimer: '<b>Achtung!</b><br/>Benützung auf eigene Gefahr! AsTeRICS Ergo darf nicht im Zusammenhang mit lebenserhaltenden Maßnahmen oder als Notfallsystem verwendet werden.',
+    i18n_click_on_app: 'Klicken Sie zum Starten auf einen der folgenden Buttons.'
 };
--- custom/bin/ARE/data/webservice/angular/global/i18n/i18n_global_en.js
+++ custom/bin/ARE/data/webservice/angular/global/i18n/i18n_global_en.js
@@ -4,5 +4,7 @@ asterics.i18n_global_en = {
     i18n_lang_en: 'English',
     i18n_available_apps: 'Available Apps',
     i18n_you_are_here: 'You are here:',
-    i18n_back: 'Back'
+    i18n_back: 'Back',
+    i18n_disclaimer: '<b>Attention!</b><br/>Use at your own risk! AsTeRICS Ergo must not be used related to life-sustaining measures or as an emergency-system.',
+    i18n_click_on_app: 'Click on one of the buttons below to start.'
 };
