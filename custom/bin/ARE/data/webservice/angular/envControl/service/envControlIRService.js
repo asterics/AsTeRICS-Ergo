3ddf02eef0ace1606c6bdf1f5c611aebd686ad49
Benjamin Klaus <klues@live.de>
2017-04-27 12:27:57 +0200

    fixed wrong detections of "isConnected" for irTrans

--- custom/bin/ARE/data/webservice/angular/envControl/service/envControlIRService.js
+++ custom/bin/ARE/data/webservice/angular/envControl/service/envControlIRService.js
@@ -1,12 +1,60 @@
 angular.module(asterics.appServices)
-    .service('envControlIRService', ['areService', '$q', function (areService, $q) {
+    .service('envControlIRService', ['areService', '$q', '$timeout', function (areService, $q, $timeout) {
         var thiz = this;
         var irTransName = 'IrTrans.1';
         var irTransActionInput = 'action';
         var learnCmdResponse = 'LEARN ';
+        var _learnWaitSeconds = 5;
+        var _learnWaitMillis = _learnWaitSeconds * 1000;
+        var _lastLearnStartTime;
         thiz.canceler = $q.defer();
 
-        thiz.irAction = function (cmd) {
+        thiz.irSend = function (cmd) {
+            awaitLastLearnTimeout().then(function () {
+                return irAction('sndhex H' + cmd);
+            });
+        };
+
+        thiz.irLearn = function () {
+            _lastLearnStartTime = new Date().getTime();
+            var def = $q.defer();
+            irAction('learn ,,,,,W' + _learnWaitSeconds).then(function (response) { //W5 means timeout of 5 seconds
+                var index = response.indexOf(learnCmdResponse);
+                if (index == -1 || response.indexOf(asterics.envControl.IRTRANS_TIMEOUT_ERROR) !== -1) {
+                    def.reject(response);
+                } else {
+                    def.resolve(response.substring(index + learnCmdResponse.length));
+                }
+            }, function error(response) {
+                def.reject(response);
+            });
+            return def.promise;
+        };
+
+        thiz.isConnected = function () {
+            var def = $q.defer();
+            awaitLastLearnTimeout().then(function () {
+                irAction('snd irtrans,ok').then(function (response) {
+                    if (response.indexOf(asterics.envControl.IRTRANS_SOCKET_ERROR) !== -1) {
+                        def.resolve(false);
+                    } else {
+                        def.resolve(true);
+                    }
+                }, function error(response) {
+                    def.resolve(false);
+                });
+            });
+            return def.promise;
+        };
+
+        //aborts a current action, unsubscribes from SSE
+        thiz.abortAction = function () {
+            thiz.canceler.resolve();
+            thiz.canceler = $q.defer();
+            return areService.unsubscribeSSE(asterics.const.ServerEventTypes.DATA_CHANNEL_TRANSMISSION);
+        };
+
+        function irAction(cmd) {
             var def = $q.defer();
             var actionString = '@IRTRANS:' + cmd;
             console.log("sending: " + actionString);
@@ -35,43 +83,23 @@ angular.module(asterics.appServices)
             return def.promise;
         };
 
-        thiz.irSend = function (cmd) {
-            return thiz.irAction('sndhex H' + cmd);
-        };
-
-        thiz.irLearn = function () {
+        function awaitLastLearnTimeout() {
             var def = $q.defer();
-            thiz.irAction('learn ,,,,,W5').then(function (response) { //W5 means timeout of 5 seconds
-                var index = response.indexOf(learnCmdResponse);
-                if (index == -1 || response.indexOf(asterics.envControl.IRTRANS_TIMEOUT_ERROR) !== -1) {
-                    def.reject(response);
+            var currentTime = new Date().getTime();
+            if (!_lastLearnStartTime) {
+                def.resolve();
+            } else {
+                var diff = currentTime - _lastLearnStartTime;
+                if (diff < _learnWaitMillis) {
+                    var waitTime = _learnWaitMillis - diff + 500;
+                    console.log("waiting " + waitTime + "ms for finishing last learn-command");
+                    $timeout(function () {
+                        def.resolve();
+                    }, waitTime);
                 } else {
-                    def.resolve(response.substring(index + learnCmdResponse.length));
+                    def.resolve();
                 }
-            }, function error(response) {
-                def.reject(response);
-            });
+            }
             return def.promise;
-        };
-
-        thiz.isConnected = function () {
-            var def = $q.defer();
-            thiz.irAction('snd irtrans,ok').then(function (response) {
-                if (response.indexOf(asterics.envControl.IRTRANS_TIMEOUT_ERROR) !== -1 || response.indexOf(asterics.envControl.IRTRANS_SOCKET_ERROR) !== -1) {
-                    def.resolve(false);
-                } else {
-                    def.resolve(true);
-                }
-            }, function error(response) {
-                def.resolve(false);
-            });
-            return def.promise;
-        };
-
-        //aborts a current action, unsubscribes from SSE
-        thiz.abortAction = function () {
-            thiz.canceler.resolve();
-            thiz.canceler = $q.defer();
-            return areService.unsubscribeSSE(asterics.const.ServerEventTypes.DATA_CHANNEL_TRANSMISSION);
         };
     }]);
