362595ead7d07fdeaa4f6a37ac362b5a3c2f39ed
Benjamin Klaus <klaus@technikum-wien.at>
2017-09-13 15:12:44 +0200

    adapted travis-script

--- travis_update_ghpages.sh
+++ travis_update_ghpages.sh
@@ -3,9 +3,6 @@
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-openssl aes-256-cbc -K $encrypted_e6e51d25be94_key -iv $encrypted_e6e51d25be94_iv -in asterics_ergo_deploy_key.enc -out asterics_ergo_deploy_key -d
-chmod 600 ./asterics_ergo_deploy_key
-eval `ssh-agent -s`
-ssh-add asterics_ergo_deploy_key
+set-up-ssh --key "$encrypted_e6e51d25be94_key" --iv "$encrypted_e6e51d25be94_iv" --path-encrypted-key "asterics_ergo_deploy_key.enc"
 
 git subtree push --prefix custom/bin/ARE/data/webservice/ origin gh-pages
