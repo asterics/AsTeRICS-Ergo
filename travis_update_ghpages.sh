f3616c72f4057bb6d69f0a2483512bdab462ba75
Benjamin Klaus <klaus@technikum-wien.at>
2017-09-13 14:56:38 +0200

    updated travis deploy script

--- travis_update_ghpages.sh
+++ travis_update_ghpages.sh
@@ -4,8 +4,8 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../asterics_ergo_deploy_key.enc -out ../asterics_ergo_deploy_key -d
-chmod 600 ../asterics_ergo_deploy_key
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ./asterics_ergo_deploy_key.enc -out ./asterics_ergo_deploy_key -d
+chmod 600 ./asterics_ergo_deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
 
