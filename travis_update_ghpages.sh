cc1633a243f5fefb6f532327cb776dad5f76dcfa
Benjamin Klaus <klaus@technikum-wien.at>
2017-09-13 16:06:16 +0200

    added echos to travis-script

--- travis_update_ghpages.sh
+++ travis_update_ghpages.sh
@@ -6,8 +6,9 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 openssl aes-256-cbc -K $encrypted_e6e51d25be94_key -iv $encrypted_e6e51d25be94_iv -in asterics_ergo_deploy_key.enc -out asterics_ergo_deploy_key -d
 chmod 600 ./asterics_ergo_deploy_key
 
+echo "adding ssh-key..."
 eval `ssh-agent -s`
 echo -e "\n" | ssh-add asterics_ergo_deploy_key
 
-
+echo "pushing to gh-pages branch..."
 git subtree push --prefix custom/bin/ARE/data/webservice/ origin gh-pages
