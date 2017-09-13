e8a4cdab61a975f8ab43615b4b368ad5d91943ee
Benjamin Klaus <klaus@technikum-wien.at>
2017-09-13 15:45:28 +0200

    copying id_rsa instead of ssh-add

--- travis_update_ghpages.sh
+++ travis_update_ghpages.sh
@@ -5,7 +5,6 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 openssl aes-256-cbc -K $encrypted_e6e51d25be94_key -iv $encrypted_e6e51d25be94_iv -in asterics_ergo_deploy_key.enc -out asterics_ergo_deploy_key -d
 chmod 600 ./asterics_ergo_deploy_key
-eval `ssh-agent -s`
-sshpass -p "" ssh-add asterics_ergo_deploy_key
+cp asterics_ergo_deploy_key ~/.ssh/id_rsa
 
 git subtree push --prefix custom/bin/ARE/data/webservice/ origin gh-pages
