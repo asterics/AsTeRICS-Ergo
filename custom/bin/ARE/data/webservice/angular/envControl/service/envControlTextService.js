bce404eb05987643ed9bd335945506f230fa77cf
Benjamin Klaus <klaus@technikum-wien.at>
2017-09-26 17:50:12 +0200

    issue#11 added method to return faq-objects by language

--- custom/bin/ARE/data/webservice/angular/envControl/service/envControlTextService.js
+++ custom/bin/ARE/data/webservice/angular/envControl/service/envControlTextService.js
@@ -1,6 +1,8 @@
 angular.module(asterics.appServices)
     .service('envControlTextService', ['utilService', function (utilService) {
         var thiz = this;
+        var _prototypes = asterics.eC.prototypes;
+
         var _additionalInstructions = {};
         _additionalInstructions[asterics.envControl.STATE_ADD_AMBLIGHT] = [
             'i18n_additional_instr_amblight'
@@ -9,5 +11,15 @@ angular.module(asterics.appServices)
         thiz.getAdditionalInstructions = function (state) {
             var list = _additionalInstructions[state];
             return list || [];
+        };
+
+        thiz.getFaqs = function (language) {
+            var faqs;
+            if (lang == 'de') {
+                faqs = asterics.envControl.faq.de;
+            } else {
+                faqs = asterics.envControl.faq.en;
+            }
+            return utilService.getObjectsFromList(_prototypes.Faq, faqs);
         };
     }]);
