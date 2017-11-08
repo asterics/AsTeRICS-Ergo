6a69f95bb79b7264631ccbf0c856bc0d18205f7c
Benjamin Klaus <klaus@technikum-wien.at>
2017-11-08 13:58:15 +0100

    issue#8 added keyword to FAQ 41

--- custom/bin/ARE/data/webservice/angular/envControl/component/help/faq/envControlFaqDe.js
+++ custom/bin/ARE/data/webservice/angular/envControl/component/help/faq/envControlFaqDe.js
@@ -245,6 +245,6 @@ asterics.envControl.faq.de = [
         id: 41,
         title: 'Beim Herunterladen der AsTeRICS-Ergo Installationsdatei und/oder beim Installieren von AsTeRICS Ergo werde ich von meinem Virenschutzprogramm gewarnt, dass das Programm nicht häufig heruntergeladen wird und auf dem Gerät Schaden anrichten könnte, dass die Ausführung der App unter Umständen ein Risiko für den PC darstelle oder eine ähnliche Warnung erscheint. Was soll ich tun?',
         language: 'de',
-        keywords: []
+        keywords: ['antivirus']
     }
 ];
