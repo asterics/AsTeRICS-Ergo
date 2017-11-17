7c6315fa11d116dbbc7aedc3f6af303bbaf58848
Benjamin Klaus <klaus@technikum-wien.at>
2017-11-17 09:50:22 +0100

    issue#103 do not show "back"-button in singlePageMode, do not show link to installation-assistant in singlePageMode

--- custom/bin/ARE/data/webservice/angular/envControl/component/help/controls/envControlHelpFs.js
+++ custom/bin/ARE/data/webservice/angular/envControl/component/help/controls/envControlHelpFs.js
@@ -3,6 +3,7 @@ angular.module(asterics.appComponents)
         bindings: {},
         controller: ['utilService', '$stateParams', '$state', function (utilService, $stateParams, $state) {
             var thiz = this;
+            thiz.singlePageMode = !!$stateParams.singlePageMode;
             thiz.cellBoardConfig = [utilService.createCellBoardItemBack()];
             thiz.fs20senderLinks = asterics.envControl.LINKS[asterics.envControl.HW_FS20_PCSENDER];
             thiz.fs20plugLinks = asterics.envControl.LINKS[asterics.envControl.HW_FS20_PLUG];
