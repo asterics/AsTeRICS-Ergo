723b50b8e86b68b4f7688f5a4c2560ab7fd66c50
Benjamin Klaus <klaus@technikum-wien.at>
2017-12-04 15:44:31 +0100

    issue#127 added all versions of irserver, start correct irserver in startscript, kill running irserver before start

--- custom/bin/ARE/tools/start_irserver.sh
+++ custom/bin/ARE/tools/start_irserver.sh
@@ -1,11 +1,16 @@
+echo "killing already running irserver..."
+sudo pkill irserver
+
 DEV_NAME=$(readlink /dev/ttyIRTrans)
 if [ -z "$DEV_NAME" ]; then
   DEV_NAME="ttyUSB0"
 fi
-echo device name is $DEV_NAME
+echo "device name is $DEV_NAME"
 
 if [ "$(dpkg --print-architecture)" == "amd64" ]; then
   sudo /opt/asterics-ergo/app/tools/irserver64 /dev/$DEV_NAME
+elif [ "$(dpkg --print-architecture)" == "armhf" ]; then
+  sudo /opt/asterics-ergo/app/tools/irserver_arm /dev/$DEV_NAME
 else
   sudo /opt/asterics-ergo/app/tools/irserver /dev/$DEV_NAME
 fi
