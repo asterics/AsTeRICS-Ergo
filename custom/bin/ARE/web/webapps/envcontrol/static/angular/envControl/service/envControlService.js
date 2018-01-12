255d32df3e256959be80e093dd78983599714bcb
Benjamin Klaus <klaus@technikum-wien.at>
2018-01-12 12:17:11 +0100

    issue#130 removed unused method for getting active hardware

--- custom/bin/ARE/web/webapps/envcontrol/static/angular/envControl/service/envControlService.js
+++ custom/bin/ARE/web/webapps/envcontrol/static/angular/envControl/service/envControlService.js
@@ -1,5 +1,5 @@
 angular.module(asterics.appServices)
-    .service('envControlService', ['areService', 'deviceIrTrans', 'deviceFs20Sender', '$q', function (areService, deviceIrTrans, deviceFs20Sender, $q) {
+    .service('envControlService', ['areService', '$q', function (areService, $q) {
         var thiz = this;
         var envModelName = 'envControl.acs';
 
@@ -9,22 +9,5 @@ angular.module(asterics.appServices)
 
         thiz.isEnvModelStarted = function () {
             return areService.isModelStarted(envModelName);
-        };
-
-        thiz.getActiveHardware = function () {
-            var def = $q.defer();
-            var devices = [];
-            var promiseIrTrans = deviceIrTrans.isConnected();
-            var promiseFs20 = deviceFs20Sender.isConnected();
-            $q.all([promiseIrTrans, promiseFs20]).then(function() {
-                if(promiseIrTrans.$$state.value) {
-                    devices.push(asterics.envControl.HW_IRTRANS_USB);
-                }
-                if(promiseFs20.$$state.value) {
-                    devices.push(asterics.envControl.HW_FS20_PCSENDER);
-                }
-                def.resolve(devices);
-            });
-            return def.promise;
         };
     }]);
