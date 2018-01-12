05b7f6254c00182641aafcd4a0eec78d4869044a
Benjamin Klaus <klaus@technikum-wien.at>
2018-01-12 12:10:49 +0100

    issue#130 moved constants for irTrans to deviceIrTrans, returning constant value on learn timeout

--- angular/envControl/service/infrared/deviceIrTrans.js
+++ angular/envControl/service/infrared/deviceIrTrans.js
@@ -1,20 +1,21 @@
 angular.module(asterics.appServices)
     .service('deviceIrTrans', ['areService', 'areWebsocketService', '$q', '$timeout', function (areService, areWebsocketService, $q) {
         var thiz = this;
+        var IRTRANS_SOCKET_ERROR = 'ERROR_SOCKET_NOT_OPEN';
+        var IRTRANS_TIMEOUT_ERROR = 'TIMEOUT ERROR';
         var irTransName = 'IrTrans.1';
         var irTransActionInput = 'action';
         var learnCmdResponse = 'LEARN ';
         var _learnWaitSeconds = 5;
-        var _learnWaitMillis = _learnWaitSeconds * 1000;
         var _testTimeout = 6000;
         var _lastLearnStartTime;
         thiz.canceler = $q.defer();
 
         thiz.getName = function() {
-            return irTransName;
+            return asterics.envControl.HW_IRTRANS_USB;
         };
 
-        thiz.irSend = function (cmd) {
+        thiz.send = function (cmd) {
             return irAction('sndhex H' + cmd);
         };
 
@@ -23,8 +24,8 @@ angular.module(asterics.appServices)
             var def = $q.defer();
             irAction('learn ,,,,,W' + _learnWaitSeconds).then(function (response) { //W5 means timeout of 5 seconds
                 var index = response.indexOf(learnCmdResponse);
-                if (index == -1 || response.indexOf(asterics.envControl.IRTRANS_TIMEOUT_ERROR) !== -1) {
-                    def.reject(response);
+                if (index == -1 || response.indexOf(IRTRANS_TIMEOUT_ERROR) !== -1) {
+                    def.reject(asterics.envControl.IRLEARN_TIMEOUT);
                 } else {
                     def.resolve(response.substring(index + learnCmdResponse.length));
                 }
@@ -37,7 +38,7 @@ angular.module(asterics.appServices)
         thiz.isConnected = function () {
             var def = $q.defer();
             irAction('snd irtrans,ok', _testTimeout).then(function (response) {
-                if (response.indexOf(asterics.envControl.IRTRANS_SOCKET_ERROR) !== -1) {
+                if (response.indexOf(IRTRANS_SOCKET_ERROR) !== -1) {
                     def.resolve(false);
                 } else {
                     def.resolve(true);
@@ -61,7 +62,7 @@ angular.module(asterics.appServices)
 
             areWebsocketService.doActionAndGetWebsocketResponse(actionFunction, thiz.canceler, timeout).then(function (response) {
                 console.log('ir response: ' + response);
-                if (response.indexOf(asterics.envControl.IRTRANS_SOCKET_ERROR) !== -1) {
+                if (response.indexOf(IRTRANS_SOCKET_ERROR) !== -1) {
                     def.reject(response);
                 } else {
                     def.resolve(response);
