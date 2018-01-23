751e93496f3475124e11776eba38329c553e463f
Benjamin Klaus <klaus@technikum-wien.at>
2018-01-23 12:05:52 +0100

    issue#130 added new component "connectionCheck" that checks for available hardware for a given device to learn

---
+++ custom/bin/ARE/web/webapps/envcontrol/static/angular/envControl/component/add/connectionCheck.js
@@ -0,0 +1,49 @@
+angular.module(asterics.appComponents)
+    .component('connectionCheck', {
+        bindings: {
+            headerI18n: '<',
+            deviceId: '<'
+        },
+        controller: ['$stateParams', 'utilService', 'envControlHelpDataService', '$translate', '$state', 'ecDeviceService', '$timeout', function ($stateParams, utilService, envControlHelpDataService, $translate, $state, ecDeviceService, $timeout) {
+            var thiz = this;
+            thiz.cellBoardConfig = [utilService.createCellBoardItemNav('i18n_back', 'arrow-left', asterics.envControl.STATE_ADD)];
+            thiz.connectedHardware = null;
+            thiz.notConnected = {};
+            var _somethingNotified = false;
+
+            thiz.$onInit = function () {
+                thiz.i18nArgDevice = {
+                    device: $translate.instant('i18n_ec_' + thiz.deviceId)
+                };
+                thiz.possibleHardware = envControlHelpDataService.getComputerConfiguredHardwarePossibilities(thiz.deviceId);
+                ecDeviceService.getOneConnectedHardware(thiz.possibleHardware).then(function (response) {
+                    if(response) {
+                        thiz.connectedHardware = response;
+                        thiz.notConnected[thiz.connectedHardware.getName()] = false;
+                        var timeout = _somethingNotified ? 500 : 0;
+                        $timeout(function () {
+                            var params = {
+                                headerI18n: thiz.headerI18n,
+                                device: thiz.deviceId,
+                                hardware: thiz.connectedHardware
+                            };
+                            $state.go(asterics.envControl.STATE_ADD_IR_DEVICE_PREDEF, params);
+                        }, timeout);
+                    } else {
+                        $timeout(function () {
+                            $state.go(asterics.envControl.STATE_NO_HARDWARE_FOUND, {
+                                headerI18n: thiz.headerI18n,
+                                device: thiz.deviceId
+                            });
+                        }, 500);
+                    }
+                }, function error () {
+
+                }, function notifiy (hardware) {
+                    _somethingNotified = true;
+                    thiz.notConnected[hardware] = true;
+                });
+            };
+        }],
+        templateUrl: "angular/envControl/component/add/connectionCheck.html"
+    });
