979374f9d4582846bd706dda263ad8e21407580d
Benjamin Klaus <klaus@technikum-wien.at>
2018-01-23 17:23:22 +0100

    issue#130 using component hardware-message, getting data from stateParams, renamed "plugDevice" to "hardware"

--- custom/bin/ARE/web/webapps/envcontrol/static/angular/envControl/component/add/envControlAddFs.js
+++ custom/bin/ARE/web/webapps/envcontrol/static/angular/envControl/component/add/envControlAddFs.js
@@ -7,20 +7,23 @@ angular.module(asterics.appComponents)
         },
         controller: ['envControlDataService', '$state', 'ecDeviceService', 'utilService', '$stateParams', 'stateUtilService', '$scope', function (envControlDataService, $state, ecDeviceService, utilService, $stateParams, stateUtilService, $scope) {
             var thiz = this;
+            thiz.device = $stateParams.device;
+            thiz.hardware = $stateParams.hardware;
+            thiz.headerI18n = $stateParams.headerI18n;
+            
             thiz.cbToAdd = $stateParams.cellBoardId || asterics.envControl.STATE_MAIN;
             thiz.stateLastPart = stateUtilService.getLastPart($state.current.name);
-            thiz.cellBoardConfig = [utilService.createCellBoardItemBack()];
+            thiz.cellBoardConfig = [utilService.createCellBoardItemNav('i18n_back', 'arrow-left', asterics.envControl.STATE_ADD)];
             thiz.isConnected = null;
             thiz.neededHardware = [asterics.envControl.HW_FS20_PCSENDER, asterics.envControl.HW_FS20_PLUG];
-            thiz.plugDevice = null;
 
             thiz.addCellBoardItemAndReturn = function () {
-                var additionalData = envControlDataService.getAdditionalDeviceData(thiz.plugDevice.getName()) || [];
-                var code = thiz.plugDevice.getNewCode(additionalData);
+                var additionalData = envControlDataService.getAdditionalDeviceData(thiz.hardware.getName()) || [];
+                var code = thiz.hardware.getNewCode(additionalData);
                 additionalData.push(code);
-                envControlDataService.setAdditionalDeviceData(additionalData, thiz.plugDevice.getName());
-                thiz.plugDevice.trainCode(code);
-                envControlDataService.addCellBoardElementPlug(thiz.selectedLabel, thiz.selectedIcon, code, thiz.cbToAdd, thiz.plugDevice.getName());
+                envControlDataService.setAdditionalDeviceData(additionalData, thiz.hardware.getName());
+                thiz.hardware.trainCode(code);
+                envControlDataService.addCellBoardElementPlug(thiz.selectedLabel, thiz.selectedIcon, code, thiz.cbToAdd, thiz.hardware.getName());
                 envControlDataService.saveData();
                 $state.go(thiz.cbToAdd);
             };
@@ -36,21 +39,6 @@ angular.module(asterics.appComponents)
             thiz.goToFs20Install = function () {
                 $state.go('home.envControl.help/install/' + asterics.envControl.HW_FS20_PCSENDER);
             };
-
-            init();
-            function init() {
-                ecDeviceService.getOneConnectedDevice(asterics.envControl.HW_GROUP_PLUG).then(function (response) {
-                    if (response) {
-                        thiz.isConnected = true;
-                        thiz.plugDevice = response;
-                    } else {
-                        $state.go(asterics.envControl.STATE_NO_HARDWARE_FOUND, {
-                            headerI18n: 'i18n_ec_fs_header',
-                            device: asterics.envControl.DEVICE_TABLELAMP
-                        });
-                    }
-                });
-            }
 
             $scope.$watch(function () {
                 return thiz.selectedLabel
