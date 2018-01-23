556594f7a4640cf8b881de50be3c2698b1896e4c
Benjamin Klaus <klaus@technikum-wien.at>
2018-01-23 17:23:58 +0100

    issue#130 refactored generation of navigation items

--- custom/bin/ARE/web/webapps/envcontrol/static/angular/envControl/component/addmenu/envControlAdd.js
+++ custom/bin/ARE/web/webapps/envcontrol/static/angular/envControl/component/addmenu/envControlAdd.js
@@ -2,17 +2,29 @@ angular.module(asterics.appComponents)
     .component('envControlAdd', {
 
         bindings: {},
-        controller: ['utilService', function (utilService) {
+        controller: ['utilService', 'envControlUtilService', function (utilService, envControlUtilService) {
             var thiz = this;
             thiz.cellBoardConfig = [utilService.createCellBoardItemNav('i18n_back', 'arrow-left', asterics.envControl.STATE_MAIN)];
             thiz.cellBoardChoose = [
-                utilService.createCellBoardItemNav('i18n_ec_amblight', 'sun-o', asterics.envControl.STATE_ADD + '.' + asterics.envControl.DEVICE_AMB_LAMP),
-                utilService.createCellBoardItemNav('i18n_ec_lamp', 'lightbulb-o', asterics.envControl.STATE_ADD_LAMP),
-                utilService.createCellBoardItemNav('i18n_ec_tv', 'television', asterics.envControl.STATE_ADD + '.' + asterics.envControl.DEVICE_TV),
-                utilService.createCellBoardItemNav('i18n_ec_dvd', 'circle', asterics.envControl.STATE_ADD + '.' + asterics.envControl.DEVICE_DVD),
-                utilService.createCellBoardItemNav('i18n_ec_hifi', 'music', asterics.envControl.STATE_ADD + '.' + asterics.envControl.DEVICE_HIFI),
+                getNavItem(asterics.envControl.DEVICE_AMB_LAMP),
+                getNavItem(asterics.envControl.DEVICE_TABLELAMP),
+                getNavItem(asterics.envControl.DEVICE_TV),
+                getNavItem(asterics.envControl.DEVICE_DVD),
+                getNavItem(asterics.envControl.DEVICE_HIFI),
                 utilService.createCellBoardItemNav('i18n_ec_more', 'plus', asterics.envControl.STATE_ADDMORE)
             ];
+
+            function getNavItem(device) {
+                var stateParams = getStateParams(device);
+                var icon = envControlUtilService.getIcon(device);
+                return utilService.createCellBoardItemNav('i18n_ec_' + device, icon, asterics.envControl.STATE_CONNECTION_CHECK, stateParams);
+            }
+
+            function getStateParams(device) {
+                return {
+                    deviceId: device
+                }
+            }
         }],
         templateUrl: "angular/envControl/component/addmenu/envControlAdd.html"
     });
