5cd5493e5f045a6478ab4298664a09d0b063247a
Benjamin Klaus <klaus@technikum-wien.at>
2018-01-31 14:08:49 +0100

    issue#130 added possibility to use html-files with language suffix for hardware - help pages

--- custom/bin/ARE/web/webapps/envcontrol/static/angular/envControl/component/help/hardware/hardwareHelpMenu.js
+++ custom/bin/ARE/web/webapps/envcontrol/static/angular/envControl/component/help/hardware/hardwareHelpMenu.js
@@ -2,12 +2,13 @@ angular.module(asterics.appComponents)
     .component('hardwareHelpMenu', {
         bindings: {
         },
-        controller: ['utilService', '$state', 'envControlHelpDataService', '$stateParams', 'envControlUtilService', function (utilService, $state, envControlHelpDataService, $stateParams, envControlUtilService) {
+        controller: ['utilService', '$state', 'envControlHelpDataService', '$stateParams', 'envControlUtilService', '$translate', '$rootScope', function (utilService, $state, envControlHelpDataService, $stateParams, envControlUtilService, $translate, $rootScope) {
             var thiz = this;
             thiz.links = asterics.envControl.LINKS;
             thiz.hardwareId = $stateParams.hardwareId;
             thiz.hardwareList = envControlHelpDataService.getHardwareWithHelp();
             thiz.cellBoardConfig = [utilService.createCellBoardItemBack()];
+            thiz.htmlPath = null;
             var _helpMapping = {};
             _helpMapping[asterics.envControl.HW_USB_CABLE_AB] = asterics.envControl.HW_IRTRANS_USB;
             _helpMapping[asterics.envControl.HW_FS20_PLUG] = asterics.envControl.HW_FS20_PCSENDER;
@@ -27,11 +28,28 @@ angular.module(asterics.appComponents)
                 if(_helpMapping[thiz.hardwareId]) {
                     thiz.hardwareId = _helpMapping[thiz.hardwareId];
                 }
+                getHtmlPath(thiz.hardwareId, $translate.use());
             };
 
-            thiz.getHtmlPath = function () {
-                return 'angular/envControl/component/help/hardware/pages/' + thiz.hardwareId + '.html'
+            function getHtmlPath(hardwareId, lang) {
+                if(!hardwareId) {
+                    return;
+                }
+                var base = 'angular/envControl/component/help/hardware/pages/';
+                var localized = base + hardwareId + '_' + lang + '.html';
+                var nonLocalized = base + hardwareId + '.html';
+                utilService.existsFile(localized).then(function (response) {
+                    if(response) {
+                        thiz.htmlPath = localized;
+                    } else {
+                        thiz.htmlPath = nonLocalized;
+                    }
+                });
             }
+
+            $rootScope.$on(asterics.const.EVENT_LANG_CHANGED, function (event, lang) {
+                getHtmlPath(thiz.hardwareId, lang);
+            });
         }],
         templateUrl: "angular/envControl/component/help/hardware/hardwareHelpMenu.html"
     });
