efa47b44114c7341b7875134e30faa8eeaf646e6
Benjamin Klaus <klaus@technikum-wien.at>
2018-03-15 16:43:25 +0100

    added new assistant in order to add a table lamp with irTrans + infrared plug

---
+++ custom/bin/ARE/web/webapps/envcontrol/static/angular/envControl/component/add/addPlugDeviceIrPlug.js
@@ -0,0 +1,39 @@
+angular.module(asterics.appComponents)
+    .component('addPlugDeviceIrPlug', {
+
+        bindings: {
+            selectedLabel: '<',
+            selectedIcon: '<'
+        },
+        controller: ['envControlDataService', '$state', 'hardwareService', 'utilService', '$stateParams', '$scope', 'envControlUtilService', function (envControlDataService, $state, hardwareService, utilService, $stateParams, $scope, envControlUtilService) {
+            var thiz = this;
+            thiz.device = $stateParams.device;
+            thiz.hardware = $stateParams.hardware;
+            thiz.headerI18n = $stateParams.headerI18n;
+            
+            thiz.cbToAdd = $stateParams.cellBoardId || asterics.envControl.STATE_MAIN;
+            thiz.cellBoardConfig = [utilService.createCellBoardItemBack()];
+            thiz.translatedDevice = envControlUtilService.getTranslatedValueObject('device', thiz.device);
+
+            thiz.addCellBoardItemAndReturn = function () {
+                var code = thiz.hardware.getRandomCode(envControlDataService.getCodes(thiz.hardware.getName()));
+                thiz.hardware.send(code);
+                envControlDataService.addCellBoardElementIr(thiz.selectedLabel, thiz.selectedIcon, code, thiz.cbToAdd, thiz.hardware.getName());
+                envControlDataService.saveData();
+                $state.go(thiz.cbToAdd);
+            };
+
+            thiz.goToHelp = function () {
+                envControlUtilService.goToHelp(thiz.hardware);
+            };
+
+            $scope.$watch(function () {
+                return thiz.selectedLabel
+            }, function () {
+                if (envControlDataService.existsLabel(thiz.selectedLabel, thiz.cbToAdd)) {
+                    thiz.selectedLabel = envControlDataService.getNonConflictingLabel(thiz.selectedLabel, thiz.cbToAdd);
+                }
+            });
+        }],
+        templateUrl: "angular/envControl/component/add/addPlugDeviceIrPlug.html"
+    });
