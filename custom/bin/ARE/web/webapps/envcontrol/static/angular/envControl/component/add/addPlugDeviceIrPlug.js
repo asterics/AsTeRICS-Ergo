6abf84abf97a3f3d621a84fd871ab816f2d78c99
Benjamin Klaus <klaus@technikum-wien.at>
2018-03-16 09:32:24 +0100

    get label and icon not from bindings, but from envControlUtilService. dont fill label for generic plug devices.

--- custom/bin/ARE/web/webapps/envcontrol/static/angular/envControl/component/add/addPlugDeviceIrPlug.js
+++ custom/bin/ARE/web/webapps/envcontrol/static/angular/envControl/component/add/addPlugDeviceIrPlug.js
@@ -1,19 +1,18 @@
 angular.module(asterics.appComponents)
     .component('addPlugDeviceIrPlug', {
 
-        bindings: {
-            selectedLabel: '<',
-            selectedIcon: '<'
-        },
-        controller: ['envControlDataService', '$state', 'hardwareService', 'utilService', '$stateParams', '$scope', 'envControlUtilService', function (envControlDataService, $state, hardwareService, utilService, $stateParams, $scope, envControlUtilService) {
+        bindings: {},
+        controller: ['envControlDataService', '$state', 'hardwareService', 'utilService', '$stateParams', '$scope', 'envControlUtilService', '$translate', function (envControlDataService, $state, hardwareService, utilService, $stateParams, $scope, envControlUtilService, $translate) {
             var thiz = this;
             thiz.device = $stateParams.device;
             thiz.hardware = $stateParams.hardware;
             thiz.headerI18n = $stateParams.headerI18n;
+            thiz.selectedLabel = thiz.device != asterics.envControl.DEVICE_PLUG_GENERIC ? $translate.instant('i18n_ec_' + thiz.device) : '';
+            thiz.selectedIcon = envControlUtilService.getIcon(thiz.device);
             
             thiz.cbToAdd = $stateParams.cellBoardId || asterics.envControl.STATE_MAIN;
             thiz.cellBoardConfig = [utilService.createCellBoardItemBack()];
-            thiz.translatedDevice = envControlUtilService.getTranslatedValueObject('device', thiz.device);
+            thiz.translatedDevice = envControlUtilService.getTranslatedValueObject('device', thiz.device + '_in_text');
 
             thiz.addCellBoardItemAndReturn = function () {
                 var code = thiz.hardware.getRandomCode(envControlDataService.getCodes(thiz.hardware.getName()));
