074c3f3d9d0453ae402d5e7035fe2a041212b864
Benjamin Klaus <klaus@technikum-wien.at>
2018-03-16 09:32:24 +0100

    get label and icon not from bindings, but from envControlUtilService. dont fill label for generic plug devices.

--- angular/envControl/component/add/envControlAddFs.js
+++ angular/envControl/component/add/envControlAddFs.js
@@ -1,15 +1,14 @@
 angular.module(asterics.appComponents)
     .component('envControlAddFs', {
 
-        bindings: {
-            selectedLabel: '<',
-            selectedIcon: '<'
-        },
-        controller: ['envControlDataService', '$state', 'hardwareService', 'utilService', '$stateParams', '$scope', 'envControlUtilService', function (envControlDataService, $state, hardwareService, utilService, $stateParams, $scope, envControlUtilService) {
+        bindings: {},
+        controller: ['envControlDataService', '$state', 'hardwareService', 'utilService', '$stateParams', '$scope', 'envControlUtilService', '$translate', function (envControlDataService, $state, hardwareService, utilService, $stateParams, $scope, envControlUtilService, $translate) {
             var thiz = this;
             thiz.device = $stateParams.device;
             thiz.hardware = $stateParams.hardware;
             thiz.headerI18n = $stateParams.headerI18n;
+            thiz.selectedLabel = thiz.device != asterics.envControl.DEVICE_PLUG_GENERIC ? $translate.instant('i18n_ec_' + thiz.device) : '';
+            thiz.selectedIcon = envControlUtilService.getIcon(thiz.device);
             
             thiz.cbToAdd = $stateParams.cellBoardId || asterics.envControl.STATE_MAIN;
             thiz.cellBoardConfig = [utilService.createCellBoardItemBack()];
